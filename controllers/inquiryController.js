4c6be47edd024e3a9dbac8be10c012e62f443bac
saqebkhan <saqeb.khan@thesouledstore.com>
2026-05-16 01:54:08 +0530

    updated app

--- controllers/inquiryController.js
+++ controllers/inquiryController.js
@@ -5,9 +5,7 @@ const nodemailer = require('nodemailer');
 
 exports.createInquiry = async (req, res) => {
     const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: false,
+        service: 'gmail',
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
--- controllers/leadController.js
+++ controllers/leadController.js
@@ -9,9 +9,7 @@ exports.createLead = async (req, res) => {
 
         // Email Notification
         const transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
-            secure: false,
+            service: 'gmail',
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS,
